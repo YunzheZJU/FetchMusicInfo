ed08f4bb04b17b9f23317be84e93437a4e545986
Yunzhe <iLoveLive@honoka.moe>
2017-09-25 00:01:07 +0800

    All done.

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ import os
 import re
 import requests
 import urllib
+import shutil
+
+
+def fetchCover(text):
+    reg = r'<div class="summary-pic">.*?(https.*.jpg).*?picAlbumBtn'
+    result = re.search(reg, text)
+    if result:
+        img_addr = result.group(1)
+        r = requests.get(img_addr, stream=True)
+        try:
+            with open(os.path.join(dir_cover, str(counter) + "." + songname + "." + img_addr.split(".")[-1]), "wb") as f:
+                for chunk in r.iter_content(chunk_size=1024):
+                    f.write(chunk)
+                    f.flush()
+        finally:
+            if f:
+                f.close()
+        return 1
+    print "Error in fetching cover."
+    return 0
 
 
 def fetchText(keyword):
@@ -33,9 +53,15 @@ def fetchText(keyword):
     reg = r'searchResult'
     # print type(text)
     # print urllib.quote(keyword)
-    if not re.search(reg, text) is None:
-        print "Error in searching."
+    if re.search(reg, text):
+        print "Error in searching keyword."
         return None
+    if fetchCover(text) == 0:
+        return None
+    # reg = r'summary-pic'
+    # if re.search(reg, text) is None:
+    #     print "Error in finding cover."
+    #     return None
     try:
         reg = r'<div class="lemma-summary" label-module="lemmaSummary">(.*?)<div class="configModuleBanner">'
         text = re.search(reg, text).group(1)
@@ -65,28 +91,33 @@ def fetchText(keyword):
         return None
 
 
-dir_music = os.path.join(os.path.abspath("."), "music")
+dir_original = os.path.join(os.path.abspath("."), "original")
 dir_text = os.path.join(os.path.abspath("."), "text")
-if not os.path.exists(dir_music):
+dir_music = os.path.join(os.path.abspath("."), "music")
+dir_cover = os.path.join(os.path.abspath("."), "cover")
+if not os.path.exists(dir_original):
+    print "No materials!"
     exit(0)
-if not os.path.exists(dir_text):
-    os.mkdir(dir_text)
-fileList = os.listdir(dir_music)
+for path in [dir_text, dir_music, dir_cover]:
+    if not os.path.exists(path):
+        os.mkdir(path)
+fileList = os.listdir(dir_original)
+if "Thumbs.db" in fileList:
+    fileList.remove("Thumbs.db")
+counter = 1
 for filename in fileList:
-    # print filename
-    # print type(filename)
-    filename = filename.decode("gbk")
-    # print filename
-    # print type(filename)
     reg = r'\d+\.(.*?)\s*-\s*(.*?)(\s|\.|\(|\xa3\xa8)'
-    keyword = re.match(reg, filename).group(2)
-    # print keyword
+    songname = re.match(reg, filename).group(2)
+    keyword = songname.decode("gbk")
+    print keyword
     # print type(keyword)
     Text = fetchText(keyword)
     if Text is None:
-        print filename + "  ERROR"
+        print songname.decode("gbk") + "  ERROR"
         continue
-    with open(os.path.join(dir_text, filename.replace(".mp3", "") + ".txt"), "w") as f:
+    with open(os.path.join(dir_text, str(counter) + "." + songname + ".txt"), "w") as f:
         f.write(Text)
-    print filename + "  OK"
+    shutil.move(os.path.join(dir_original, filename), os.path.join(dir_music, str(counter) + "." + songname + ".mp3"))
+    print songname.decode("gbk") + "  OK"
+    counter += 1
 
